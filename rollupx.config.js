6c1fc2b719d8b3d8ab7bab150bd330dc39cf5e6a
yujinpan <1192878390@qq.com>
2023-11-03 15:17:23 +0800

    Update rollupx.config.js

--- rollupx.config.js
+++ rollupx.config.js
@@ -2,7 +2,7 @@ module.exports = {
   banner:
     '/*!\n' +
     ` * el-popover v${require('./package.json').version}\n` +
-    ` * (c) 2019-${new Date().getFullYear()} yujinpan\n` +
+    ` * (c) 2023-${new Date().getFullYear()} yujinpan\n` +
     ' * Released under the MIT License.\n' +
     ' */\n',
   outputDir: 'lib',
